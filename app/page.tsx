9f08c5ee10397360410cba68fd06354bd2c20bab
Jennifer Rohl <jnrohl30@gmail.com>
2025-11-02 19:39:21 -0500

    fix: restore valid homepage route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+// app/page.tsx
 import { Suspense } from "react";
 import JennyBeesCreation from "../components/JennyBeesCreation";
 
-// Helps when using useSearchParams in client components
 export const dynamic = "force-dynamic";
 
 export default function Page() {
